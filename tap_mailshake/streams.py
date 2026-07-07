c47e7421fd04c1a41ab169307a649c102ad83a6a
rsaha-qlik <vpz@qlik.com>
2026-07-07 17:58:35 +0530

    addressed changes

--- tap_mailshake/streams.py
+++ tap_mailshake/streams.py
@@ -15,72 +15,72 @@ class StreamConfig(TypedDict, total=False):
 
 
 STREAMS: dict[str, StreamConfig] = {
-    "campaigns": {
-        "path": "campaigns/list",
-        "key_properties": ["id"],
-        "replication_method": "INCREMENTAL",
-        "replication_keys": ["created"],
-        "data_key": "results",
-        "children": {
-            "recipients": {
-                "path": "recipients/list",
-                "key_properties": ["id"],
-                "replication_method": "INCREMENTAL",
-                "replication_keys": ["created"],
-                "parent": "campaigns",
-                "params": {"campaignID": "<parent_id>"},
-                "data_key": "results",
+    'campaigns': {
+        'path': 'campaigns/list',
+        'key_properties': ['id'],
+        'replication_method': 'INCREMENTAL',
+        'replication_keys': ['created'],
+        'data_key': 'results',
+        'children': {
+            'recipients': {
+                'path': 'recipients/list',
+                'key_properties': ['id'],
+                'replication_method': 'INCREMENTAL',
+                'replication_keys': ['created'],
+                'parent': 'campaigns',
+                'params': {'campaignID': '<parent_id>'},
+                'data_key': 'results',
             }
         },
     },
-    "leads": {
-        "path": "leads/list",
-        "key_properties": ["id"],
-        "replication_method": "INCREMENTAL",
-        "replication_keys": ["created"],
-        "data_key": "results",
+    'leads': {
+        'path': 'leads/list',
+        'key_properties': ['id'],
+        'replication_method': 'INCREMENTAL',
+        'replication_keys': ['created'],
+        'data_key': 'results',
     },
-    "senders": {
-        "path": "senders/list",
-        "key_properties": ["id"],
-        "replication_method": "INCREMENTAL",
-        "replication_keys": ["created"],
-        "data_key": "results",
+    'senders': {
+        'path': 'senders/list',
+        'key_properties': ['id'],
+        'replication_method': 'INCREMENTAL',
+        'replication_keys': ['created'],
+        'data_key': 'results',
     },
-    "team_members": {
-        "path": "team/list-members",
-        "key_properties": ["id"],
-        "replication_method": "FULL_TABLE",
-        "replication_keys": [],
-        "data_key": "results",
+    'team_members': {
+        'path': 'team/list-members',
+        'key_properties': ['id'],
+        'replication_method': 'FULL_TABLE',
+        'replication_keys': [],
+        'data_key': 'results',
     },
-    "sent_messages": {
-        "path": "activity/sent",
-        "key_properties": ["id"],
-        "replication_method": "INCREMENTAL",
-        "replication_keys": ["actionDate"],
-        "data_key": "results",
+    'sent_messages': {
+        'path': 'activity/sent',
+        'key_properties': ['id'],
+        'replication_method': 'INCREMENTAL',
+        'replication_keys': ['actionDate'],
+        'data_key': 'results',
     },
-    "opens": {
-        "path": "activity/opens",
-        "key_properties": ["id"],
-        "replication_method": "INCREMENTAL",
-        "replication_keys": ["actionDate"],
-        "data_key": "results",
+    'opens': {
+        'path': 'activity/opens',
+        'key_properties': ['id'],
+        'replication_method': 'INCREMENTAL',
+        'replication_keys': ['actionDate'],
+        'data_key': 'results',
     },
-    "clicks": {
-        "path": "activity/clicks",
-        "key_properties": ["id"],
-        "replication_method": "INCREMENTAL",
-        "replication_keys": ["actionDate"],
-        "data_key": "results",
+    'clicks': {
+        'path': 'activity/clicks',
+        'key_properties': ['id'],
+        'replication_method': 'INCREMENTAL',
+        'replication_keys': ['actionDate'],
+        'data_key': 'results',
     },
-    "replies": {
-        "path": "activity/replies",
-        "key_properties": ["id"],
-        "replication_method": "INCREMENTAL",
-        "replication_keys": ["actionDate"],
-        "data_key": "results",
+    'replies': {
+        'path': 'activity/replies',
+        'key_properties': ['id'],
+        'replication_method': 'INCREMENTAL',
+        'replication_keys': ['actionDate'],
+        'data_key': 'results',
     },
 }
 
@@ -90,16 +90,16 @@ def flatten_streams() -> dict[str, StreamConfig]:
     flat: dict[str, StreamConfig] = {}
     for stream_name, config in STREAMS.items():
         flat[stream_name] = {
-            "key_properties": config.get("key_properties"),
-            "replication_method": config.get("replication_method"),
-            "replication_keys": config.get("replication_keys"),
-            "parent": config.get("parent"),
+            'key_properties': config.get('key_properties'),
+            'replication_method': config.get('replication_method'),
+            'replication_keys': config.get('replication_keys'),
+            'parent': config.get('parent'),
         }
-        for child_name, child_config in config.get("children", {}).items():
+        for child_name, child_config in config.get('children', {}).items():
             flat[child_name] = {
-                "key_properties": child_config.get("key_properties"),
-                "replication_method": child_config.get("replication_method"),
-                "replication_keys": child_config.get("replication_keys"),
-                "parent": stream_name,
+                'key_properties': child_config.get('key_properties'),
+                'replication_method': child_config.get('replication_method'),
+                'replication_keys': child_config.get('replication_keys'),
+                'parent': stream_name,
             }
     return flat
